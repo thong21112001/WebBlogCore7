7f9a7d6a37e2dbe070fce3f70b5b23a3eab6fb9f
PanPan <quangthong211101@gmail.com>
2023-08-07 15:12:15 +0700

    Add project files.

---
+++ WebBlog/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebBlog.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        //Relationship
+        public List<Post> Posts { get; set; }
+    }
+}
